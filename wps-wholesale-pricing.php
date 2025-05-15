8a8fccb7c25fb3bf9d1ed290d1c89ba18ac843d0
George <gtsiokos@gmail.com>
2025-05-15 21:32:54 +0300

    added user_role check

--- wps-wholesale-pricing.php
+++ wps-wholesale-pricing.php
@@ -11,10 +11,29 @@
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 class Wps_Wholesale_Pricing {
 
 	public static function init() {
-		add_filter( 'wwpp_pre_get_post__in', array( __CLASS__, 'wwpp_pre_get_post__in' ), 10, 2 );
+		add_action( 'plugins_loaded', array( __CLASS__, 'plugins_loaded' ) );
+	}
+
+	public static function plugins_loaded() {
+		$current_logged_user = wp_get_current_user();
+		$current_user_id = $current_logged_user->ID;
+		$is_wholesale_user = false;
+		if ( $current_user_id ) {
+			$current_user_roles = $current_logged_user->roles;
+			$wholesale_roles = apply_filters( 'wps_wholesale_pricing_wholesale_roles', array( 'wholesale_customer' ) );
+			foreach ( $current_user_roles as $role ) {
+				if ( in_array( $role, $wholesale_roles ) ) {
+					$is_wholesale_user = true;
+				}
+			}
+		}
+		if ( $is_wholesale_user ) {
+			add_filter( 'wwpp_pre_get_post__in', array( __CLASS__, 'wwpp_pre_get_post__in' ), 10, 2 );
+		}
 	}
 
 	public static function wwpp_pre_get_post__in( $wwpp_products, $query_args ) {
@@ -23,7 +42,7 @@ class Wps_Wholesale_Pricing {
 				require_once 'class-engine-stage-wholesale-pricing.php';
 			}
 		}
-	
+
 		return $wwpp_products;
 	}
 } Wps_Wholesale_Pricing::init();
